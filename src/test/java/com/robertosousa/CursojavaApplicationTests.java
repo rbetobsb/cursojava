ea2f4c01b511d199abc8efce6c9716a6d3a941ea
rbetobsb <roberto.rbetobsb@gmail.com>
2020-12-29 23:45:49 -0300

    Primeiro projeto

---
+++ src/test/java/com/robertosousa/CursojavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.robertosousa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursojavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
